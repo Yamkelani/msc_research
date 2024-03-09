96b72ee67737f66d5c273414e7920694f93e6561
Yamkelani <yamzinm@gmail.com>
2024-03-09 10:51:26 +0200

    Work in progress: Still need to join the two tables to create one table that will be used to generate the features.

--- rul_prediction/src/matlab_code/scripts/rul_prediction_v1.m
+++ rul_prediction/src/matlab_code/scripts/rul_prediction_v1.m
@@ -4,20 +4,16 @@
 clc;
 clear;
 
-function dataset = rename_columns(dataset, prefix)
-    for pos=1:numel(dataset.Properties.VariableNames)
-        dataset.Properties.VariableNames{pos} = prefix + dataset.Properties.VariableNames{pos}; 
-end
 
 %Data path
 
-base_path = "/home/yamukelani/source_code/msc_research/rul_prediction/src/datasets";
-bat_discharge_path = base_path+"battery_discharging_data.csv";
-bat_charge_path = base_path +"battery_charging_data.csv";
+base_path = "/home/yamukelani/source_code/msc_research/rul_prediction/src";
+bat_discharge_path = base_path+"/datasets/battery_discharging_data.csv";
+bat_charge_path = base_path +"/datasets/battery_charging_data.csv";
 
 
-%Create the folder if it does not exist
-battery_folder = "BatteryFolder";
+%Create the folder if it does not already exist
+battery_folder = base_path+"/models";
 
 if ~exist(battery_folder,"dir")
     mkdir(battery_folder);
@@ -27,9 +23,11 @@ battery_charge_data = readtable(bat_charge_path);
 battery_discharge_data = readtable(bat_discharge_path);
 
 %Rename columns
-battery_charge_data = rename_columns(battery_charge_data , "charge")
-battery_dicharge_data = rename_columns(battery_discharge_data , "discharge")
+battery_charge_data = rename_columns(battery_charge_data , 'charge');
+battery_discharge_data = rename_columns(battery_discharge_data , 'discharge');
 
+disp(battery_charge_data.Properties.VariableNames)
+disp(battery_discharge_data.Properties.VariableNames)
 
 disp("Battery discharge data is:")
 disp(size(battery_discharge_data))
@@ -41,7 +39,7 @@ disp(size(battery_charge_data))
 disp(head(battery_charge_data,3))
 
 %Prepare data for modelling
-
+battery_data = join(battery_charge_data, battery_discharge_data)
 head(battery_data, 5);
 
 bat_y = battery_data(:,[10]);
@@ -53,8 +51,19 @@ bat_y = battery_data(:,[10]);
 %head(Xtrain,10)
 
 [test] = battery_data([1:(0.4*height(battery_data))],[4 5 6 7 8 9 10]);
-[Xtest] =train(:,[1:6]);
-%[Ytest] =train(:,[7]);
+[Xtest] = train(:,[1:6]);
+%[Ytest] = train(:,[7]);
 
 model_name = "BatteryCellRULPrediction";
 
+function new_dataset = rename_columns(dataset, prefix)
+    num_of_variables = length(dataset.Properties.VariableNames);
+    varNames = cell(1, num_of_variables);
+
+    for pos=1:num_of_variables
+        varNames{pos} = [append(prefix,'_') dataset.Properties.VariableNames{pos}]; 
+    end
+
+    dataset.Properties.VariableNames = varNames;
+    new_dataset = dataset;
+end
