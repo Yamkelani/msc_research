fc226e43d8477972ab10f87e58b2f7768c0a7301
Yamkelani <yamzinm@gmail.com>
2024-06-05 14:28:28 +0200

    Adding rul simulink model and generated code

---
+++ rul_prediction/src/matlab_code/scripts/rul_data_acquisition_complete_needs_debugging_ert_rtw/MW_c28xx_board.c
@@ -0,0 +1,51 @@
+#include "c2000BoardSupport.h"
+#include "F2802x_Device.h"
+#include "f2802x_examples.h"
+#include "f2802x_globalprototypes.h"
+#include "rtwtypes.h"
+#include "rul_data_acquisition_complete_needs_debugging.h"
+#include "rul_data_acquisition_complete_needs_debugging_private.h"
+
+void init_board (void)
+{
+  DisableDog();
+  EALLOW;
+  SysCtrlRegs.PCLKCR0.bit.ADCENCLK = 1U;/* Enable ADC peripheral clock*/
+  (*Device_cal)();
+  SysCtrlRegs.PCLKCR0.bit.ADCENCLK = 0U;/* Return ADC clock to original state*/
+  EDIS;
+
+  /* Select Internal Oscillator 1 as Clock Source (default), and turn off all unused clocks to
+   * conserve power.
+   */
+  IntOsc1Sel();
+
+  /* Initialize the PLL control: PLLCR and DIVSEL
+   * DSP28_PLLCR and DSP28_DIVSEL are defined in DSP2802x_Examples.h
+   */
+  InitPll(12,2);
+  InitPeripheralClocks();
+  EALLOW;
+
+  /* Configure low speed peripheral clocks */
+  SysCtrlRegs.LOSPCP.all = 0U;
+  EDIS;
+
+  /* Disable and clear all CPU interrupts */
+  DINT;
+  IER = 0x0000U;
+  IFR = 0x0000U;
+  InitPieCtrl();
+  InitPieVectTable();
+  InitCpuTimers();
+
+#ifdef CPU1
+#endif                                 // #ifdef CPU1
+
+  /* initial GPIO qualification settings.... */
+  EALLOW;
+  GpioCtrlRegs.GPAQSEL1.all = 0x0U;
+  GpioCtrlRegs.GPAQSEL2.all = 0x0U;
+  GpioCtrlRegs.GPBQSEL1.all = 0x0U;
+  EDIS;
+}
---
+++ rul_prediction/src/matlab_code/scripts/rul_data_acquisition_complete_needs_debugging_ert_rtw/rul_data_acquisition_complete_needs_debugging.d
@@ -0,0 +1,102 @@
+rul_data_acquisition_complete_needs_debugging.obj: C:/Users/yamzi/source_code/msc_research/rul_prediction/src/matlab_code/scripts/rul_data_acquisition_complete_needs_debugging_ert_rtw/rul_data_acquisition_complete_needs_debugging.c
+rul_data_acquisition_complete_needs_debugging.obj: C:\Users\yamzi\source_code\msc_research\rul_prediction\src\matlab_code\scripts\rul_data_acquisition_complete_needs_debugging_ert_rtw\rul_data_acquisition_complete_needs_debugging.h
+rul_data_acquisition_complete_needs_debugging.obj: C:\Users\yamzi\source_code\msc_research\rul_prediction\src\matlab_code\scripts\rul_data_acquisition_complete_needs_debugging_ert_rtw\rtwtypes.h
+rul_data_acquisition_complete_needs_debugging.obj: C:/PROGRA~1/MATLAB/R2023b/simulink/include/rtw_extmode.h
+rul_data_acquisition_complete_needs_debugging.obj: C:/PROGRA~1/MATLAB/R2023b/simulink/include/sysran_types.h
+rul_data_acquisition_complete_needs_debugging.obj: C:/PROGRA~1/MATLAB/R2023b/toolbox/c2b/tic2000_shared/src/c2000BoardSupport.h
+rul_data_acquisition_complete_needs_debugging.obj: C:/PROGRA~3/MATLAB/tic2000/3P778C~1.INS/TIC28X~1.INS/TI-CGT~1.LTS/include/stdbool.h
+rul_data_acquisition_complete_needs_debugging.obj: C:/PROGRA~3/MATLAB/tic2000/3P778C~1.INS/TIC28X~1.INS/TI-CGT~1.LTS/include/_ti_config.h
+rul_data_acquisition_complete_needs_debugging.obj: C:/PROGRA~3/MATLAB/tic2000/3P778C~1.INS/TIC28X~1.INS/TI-CGT~1.LTS/include/linkage.h
+rul_data_acquisition_complete_needs_debugging.obj: C:/ti/CONTRO~1/DEVICE~1/f2802x/v230/F2802X~4/include/F2802x_Device.h
+rul_data_acquisition_complete_needs_debugging.obj: C:/PROGRA~3/MATLAB/tic2000/3P778C~1.INS/TIC28X~1.INS/TI-CGT~1.LTS/include/assert.h
+rul_data_acquisition_complete_needs_debugging.obj: C:/PROGRA~3/MATLAB/tic2000/3P778C~1.INS/TIC28X~1.INS/TI-CGT~1.LTS/include/stdarg.h
+rul_data_acquisition_complete_needs_debugging.obj: C:/PROGRA~3/MATLAB/tic2000/3P778C~1.INS/TIC28X~1.INS/TI-CGT~1.LTS/include/sys/_types.h
+rul_data_acquisition_complete_needs_debugging.obj: C:/PROGRA~3/MATLAB/tic2000/3P778C~1.INS/TIC28X~1.INS/TI-CGT~1.LTS/include/sys/cdefs.h
+rul_data_acquisition_complete_needs_debugging.obj: C:/PROGRA~3/MATLAB/tic2000/3P778C~1.INS/TIC28X~1.INS/TI-CGT~1.LTS/include/machine/_types.h
+rul_data_acquisition_complete_needs_debugging.obj: C:/PROGRA~3/MATLAB/tic2000/3P778C~1.INS/TIC28X~1.INS/TI-CGT~1.LTS/include/stddef.h
+rul_data_acquisition_complete_needs_debugging.obj: C:/PROGRA~3/MATLAB/tic2000/3P778C~1.INS/TIC28X~1.INS/TI-CGT~1.LTS/include/stdint.h
+rul_data_acquisition_complete_needs_debugging.obj: C:/PROGRA~3/MATLAB/tic2000/3P778C~1.INS/TIC28X~1.INS/TI-CGT~1.LTS/include/_stdint40.h
+rul_data_acquisition_complete_needs_debugging.obj: C:/PROGRA~3/MATLAB/tic2000/3P778C~1.INS/TIC28X~1.INS/TI-CGT~1.LTS/include/sys/stdint.h
+rul_data_acquisition_complete_needs_debugging.obj: C:/PROGRA~3/MATLAB/tic2000/3P778C~1.INS/TIC28X~1.INS/TI-CGT~1.LTS/include/machine/_stdint.h
+rul_data_acquisition_complete_needs_debugging.obj: C:/PROGRA~3/MATLAB/tic2000/3P778C~1.INS/TIC28X~1.INS/TI-CGT~1.LTS/include/sys/_stdint.h
+rul_data_acquisition_complete_needs_debugging.obj: C:\ti\CONTRO~1\DEVICE~1\f2802x\v230\F2802X~4\include\F2802x_Adc.h
+rul_data_acquisition_complete_needs_debugging.obj: C:\ti\CONTRO~1\DEVICE~1\f2802x\v230\F2802X~4\include\F2802x_BootVars.h
+rul_data_acquisition_complete_needs_debugging.obj: C:\ti\CONTRO~1\DEVICE~1\f2802x\v230\F2802X~4\include\F2802x_DevEmu.h
+rul_data_acquisition_complete_needs_debugging.obj: C:\ti\CONTRO~1\DEVICE~1\f2802x\v230\F2802X~4\include\F2802x_Comp.h
+rul_data_acquisition_complete_needs_debugging.obj: C:\ti\CONTRO~1\DEVICE~1\f2802x\v230\F2802X~4\include\F2802x_CpuTimers.h
+rul_data_acquisition_complete_needs_debugging.obj: C:\ti\CONTRO~1\DEVICE~1\f2802x\v230\F2802X~4\include\F2802x_ECap.h
+rul_data_acquisition_complete_needs_debugging.obj: C:\ti\CONTRO~1\DEVICE~1\f2802x\v230\F2802X~4\include\F2802x_EPwm.h
+rul_data_acquisition_complete_needs_debugging.obj: C:\ti\CONTRO~1\DEVICE~1\f2802x\v230\F2802X~4\include\F2802x_Gpio.h
+rul_data_acquisition_complete_needs_debugging.obj: C:\ti\CONTRO~1\DEVICE~1\f2802x\v230\F2802X~4\include\F2802x_I2c.h
+rul_data_acquisition_complete_needs_debugging.obj: C:\ti\CONTRO~1\DEVICE~1\f2802x\v230\F2802X~4\include\F2802x_NmiIntrupt.h
+rul_data_acquisition_complete_needs_debugging.obj: C:\ti\CONTRO~1\DEVICE~1\f2802x\v230\F2802X~4\include\F2802x_PieCtrl.h
+rul_data_acquisition_complete_needs_debugging.obj: C:\ti\CONTRO~1\DEVICE~1\f2802x\v230\F2802X~4\include\F2802x_PieVect.h
+rul_data_acquisition_complete_needs_debugging.obj: C:\ti\CONTRO~1\DEVICE~1\f2802x\v230\F2802X~4\include\F2802x_Spi.h
+rul_data_acquisition_complete_needs_debugging.obj: C:\ti\CONTRO~1\DEVICE~1\f2802x\v230\F2802X~4\include\F2802x_Sci.h
+rul_data_acquisition_complete_needs_debugging.obj: C:\ti\CONTRO~1\DEVICE~1\f2802x\v230\F2802X~4\include\F2802x_SysCtrl.h
+rul_data_acquisition_complete_needs_debugging.obj: C:\ti\CONTRO~1\DEVICE~1\f2802x\v230\F2802X~4\include\F2802x_XIntrupt.h
+rul_data_acquisition_complete_needs_debugging.obj: C:/ti/CONTRO~1/DEVICE~1/f2802x/v230/F2802X~1/include/f2802x_examples.h
+rul_data_acquisition_complete_needs_debugging.obj: C:\ti\CONTRO~1\DEVICE~1\f2802x\v230\F2802X~1\include\f2802x_globalprototypes.h
+rul_data_acquisition_complete_needs_debugging.obj: C:\ti\CONTRO~1\DEVICE~1\f2802x\v230\F2802X~1\include\f2802x_epwm_defines.h
+rul_data_acquisition_complete_needs_debugging.obj: C:\ti\CONTRO~1\DEVICE~1\f2802x\v230\F2802X~1\include\f2802x_i2c_defines.h
+rul_data_acquisition_complete_needs_debugging.obj: C:\ti\CONTRO~1\DEVICE~1\f2802x\v230\F2802X~1\include\f2802x_defaultisr.h
+rul_data_acquisition_complete_needs_debugging.obj: C:\Users\yamzi\source_code\msc_research\rul_prediction\src\matlab_code\scripts\rul_data_acquisition_complete_needs_debugging_ert_rtw\rul_data_acquisition_complete_needs_debugging_types.h
+rul_data_acquisition_complete_needs_debugging.obj: C:/PROGRA~3/MATLAB/tic2000/3P778C~1.INS/TIC28X~1.INS/TI-CGT~1.LTS/include/string.h
+rul_data_acquisition_complete_needs_debugging.obj: C:/PROGRA~3/MATLAB/tic2000/3P778C~1.INS/TIC28X~1.INS/TI-CGT~1.LTS/include/xlocale/_string.h
+rul_data_acquisition_complete_needs_debugging.obj: C:\Users\yamzi\source_code\msc_research\rul_prediction\src\matlab_code\scripts\rul_data_acquisition_complete_needs_debugging_ert_rtw\MW_target_hardware_resources.h
+rul_data_acquisition_complete_needs_debugging.obj: C:/PROGRA~1/MATLAB/R2023b/toolbox/c2b/tic2000_shared/src/c2000SchedulerTimer.h
+rul_data_acquisition_complete_needs_debugging.obj: C:\Users\yamzi\source_code\msc_research\rul_prediction\src\matlab_code\scripts\rul_data_acquisition_complete_needs_debugging_ert_rtw\rul_data_acquisition_complete_needs_debugging_private.h
+rul_data_acquisition_complete_needs_debugging.obj: C:\Users\yamzi\source_code\msc_research\rul_prediction\src\matlab_code\scripts\rul_data_acquisition_complete_needs_debugging_ert_rtw\multiword_types.h
+rul_data_acquisition_complete_needs_debugging.obj: C:/PROGRA~3/MATLAB/tic2000/3P778C~1.INS/TIC28X~1.INS/TI-CGT~1.LTS/include/limits.h
+
+C:/Users/yamzi/source_code/msc_research/rul_prediction/src/matlab_code/scripts/rul_data_acquisition_complete_needs_debugging_ert_rtw/rul_data_acquisition_complete_needs_debugging.c:
+C:\Users\yamzi\source_code\msc_research\rul_prediction\src\matlab_code\scripts\rul_data_acquisition_complete_needs_debugging_ert_rtw\rul_data_acquisition_complete_needs_debugging.h:
+C:\Users\yamzi\source_code\msc_research\rul_prediction\src\matlab_code\scripts\rul_data_acquisition_complete_needs_debugging_ert_rtw\rtwtypes.h:
+C:/PROGRA~1/MATLAB/R2023b/simulink/include/rtw_extmode.h:
+C:/PROGRA~1/MATLAB/R2023b/simulink/include/sysran_types.h:
+C:/PROGRA~1/MATLAB/R2023b/toolbox/c2b/tic2000_shared/src/c2000BoardSupport.h:
+C:/PROGRA~3/MATLAB/tic2000/3P778C~1.INS/TIC28X~1.INS/TI-CGT~1.LTS/include/stdbool.h:
+C:/PROGRA~3/MATLAB/tic2000/3P778C~1.INS/TIC28X~1.INS/TI-CGT~1.LTS/include/_ti_config.h:
+C:/PROGRA~3/MATLAB/tic2000/3P778C~1.INS/TIC28X~1.INS/TI-CGT~1.LTS/include/linkage.h:
+C:/ti/CONTRO~1/DEVICE~1/f2802x/v230/F2802X~4/include/F2802x_Device.h:
+C:/PROGRA~3/MATLAB/tic2000/3P778C~1.INS/TIC28X~1.INS/TI-CGT~1.LTS/include/assert.h:
+C:/PROGRA~3/MATLAB/tic2000/3P778C~1.INS/TIC28X~1.INS/TI-CGT~1.LTS/include/stdarg.h:
+C:/PROGRA~3/MATLAB/tic2000/3P778C~1.INS/TIC28X~1.INS/TI-CGT~1.LTS/include/sys/_types.h:
+C:/PROGRA~3/MATLAB/tic2000/3P778C~1.INS/TIC28X~1.INS/TI-CGT~1.LTS/include/sys/cdefs.h:
+C:/PROGRA~3/MATLAB/tic2000/3P778C~1.INS/TIC28X~1.INS/TI-CGT~1.LTS/include/machine/_types.h:
+C:/PROGRA~3/MATLAB/tic2000/3P778C~1.INS/TIC28X~1.INS/TI-CGT~1.LTS/include/stddef.h:
+C:/PROGRA~3/MATLAB/tic2000/3P778C~1.INS/TIC28X~1.INS/TI-CGT~1.LTS/include/stdint.h:
+C:/PROGRA~3/MATLAB/tic2000/3P778C~1.INS/TIC28X~1.INS/TI-CGT~1.LTS/include/_stdint40.h:
+C:/PROGRA~3/MATLAB/tic2000/3P778C~1.INS/TIC28X~1.INS/TI-CGT~1.LTS/include/sys/stdint.h:
+C:/PROGRA~3/MATLAB/tic2000/3P778C~1.INS/TIC28X~1.INS/TI-CGT~1.LTS/include/machine/_stdint.h:
+C:/PROGRA~3/MATLAB/tic2000/3P778C~1.INS/TIC28X~1.INS/TI-CGT~1.LTS/include/sys/_stdint.h:
+C:\ti\CONTRO~1\DEVICE~1\f2802x\v230\F2802X~4\include\F2802x_Adc.h:
+C:\ti\CONTRO~1\DEVICE~1\f2802x\v230\F2802X~4\include\F2802x_BootVars.h:
+C:\ti\CONTRO~1\DEVICE~1\f2802x\v230\F2802X~4\include\F2802x_DevEmu.h:
+C:\ti\CONTRO~1\DEVICE~1\f2802x\v230\F2802X~4\include\F2802x_Comp.h:
+C:\ti\CONTRO~1\DEVICE~1\f2802x\v230\F2802X~4\include\F2802x_CpuTimers.h:
+C:\ti\CONTRO~1\DEVICE~1\f2802x\v230\F2802X~4\include\F2802x_ECap.h:
+C:\ti\CONTRO~1\DEVICE~1\f2802x\v230\F2802X~4\include\F2802x_EPwm.h:
+C:\ti\CONTRO~1\DEVICE~1\f2802x\v230\F2802X~4\include\F2802x_Gpio.h:
+C:\ti\CONTRO~1\DEVICE~1\f2802x\v230\F2802X~4\include\F2802x_I2c.h:
+C:\ti\CONTRO~1\DEVICE~1\f2802x\v230\F2802X~4\include\F2802x_NmiIntrupt.h:
+C:\ti\CONTRO~1\DEVICE~1\f2802x\v230\F2802X~4\include\F2802x_PieCtrl.h:
+C:\ti\CONTRO~1\DEVICE~1\f2802x\v230\F2802X~4\include\F2802x_PieVect.h:
+C:\ti\CONTRO~1\DEVICE~1\f2802x\v230\F2802X~4\include\F2802x_Spi.h:
+C:\ti\CONTRO~1\DEVICE~1\f2802x\v230\F2802X~4\include\F2802x_Sci.h:
+C:\ti\CONTRO~1\DEVICE~1\f2802x\v230\F2802X~4\include\F2802x_SysCtrl.h:
+C:\ti\CONTRO~1\DEVICE~1\f2802x\v230\F2802X~4\include\F2802x_XIntrupt.h:
+C:/ti/CONTRO~1/DEVICE~1/f2802x/v230/F2802X~1/include/f2802x_examples.h:
+C:\ti\CONTRO~1\DEVICE~1\f2802x\v230\F2802X~1\include\f2802x_globalprototypes.h:
+C:\ti\CONTRO~1\DEVICE~1\f2802x\v230\F2802X~1\include\f2802x_epwm_defines.h:
+C:\ti\CONTRO~1\DEVICE~1\f2802x\v230\F2802X~1\include\f2802x_i2c_defines.h:
+C:\ti\CONTRO~1\DEVICE~1\f2802x\v230\F2802X~1\include\f2802x_defaultisr.h:
+C:\Users\yamzi\source_code\msc_research\rul_prediction\src\matlab_code\scripts\rul_data_acquisition_complete_needs_debugging_ert_rtw\rul_data_acquisition_complete_needs_debugging_types.h:
+C:/PROGRA~3/MATLAB/tic2000/3P778C~1.INS/TIC28X~1.INS/TI-CGT~1.LTS/include/string.h:
+C:/PROGRA~3/MATLAB/tic2000/3P778C~1.INS/TIC28X~1.INS/TI-CGT~1.LTS/include/xlocale/_string.h:
+C:\Users\yamzi\source_code\msc_research\rul_prediction\src\matlab_code\scripts\rul_data_acquisition_complete_needs_debugging_ert_rtw\MW_target_hardware_resources.h:
+C:/PROGRA~1/MATLAB/R2023b/toolbox/c2b/tic2000_shared/src/c2000SchedulerTimer.h:
+C:\Users\yamzi\source_code\msc_research\rul_prediction\src\matlab_code\scripts\rul_data_acquisition_complete_needs_debugging_ert_rtw\rul_data_acquisition_complete_needs_debugging_private.h:
+C:\Users\yamzi\source_code\msc_research\rul_prediction\src\matlab_code\scripts\rul_data_acquisition_complete_needs_debugging_ert_rtw\multiword_types.h:
+C:/PROGRA~3/MATLAB/tic2000/3P778C~1.INS/TIC28X~1.INS/TI-CGT~1.LTS/include/limits.h:
+
