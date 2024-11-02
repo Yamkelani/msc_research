1ce6d2980d61da54ca7a6e00cfe45a81f8a3ccf3
Yamkelani <yamzinm@gmail.com>
2024-11-02 23:37:22 +0200

    Adding the battery tests code updates

--- rul_prediction/src/python_code/commons/itech_3600_regen_power_supply_controller.py
+++ rul_prediction/src/python_code/commons/itech_3600_regen_power_supply_controller.py
@@ -1,15 +1,27 @@
 import pyvisa as pvs
 import pandas as pd
-import time
+import logging
+import datetime
+import json
 
 #Define the constants
 
 
 REGEN_POWER_SUPPLY_ADDR_ITECH_IT6012C = 'USB0::0x2EC7::0x6000::804465012787740004::INSTR'
 REGEN_POWER_SUPPLY_ADDR_ITECH_M3622 = 'USB0::0x2EC7::0x3622::803359022767110013::INSTR'
+REGEN_POWER_SUPPLY_ADDR_ITECH_AEL_M3622  = 'USB0::0x2EC7::0x3622::803359022797450003::INSTR'
 CELL_MAX_VOLTAGE = 5.0
 CELL_MAX_CURRENT = 2.0
 CAPACITY = 6.8
+
+logger = logging.getLogger(__name__)
+FileOutputHandler = logging.FileHandler('logs.log')
+
+logger.addHandler(FileOutputHandler)
+
+logger.warning("Warning.")
+
+
 
 class PVStorageEmulator:
 
@@ -48,33 +60,46 @@ class PVStorageEmulator:
     def get_pv_current():
         pass
 
-    def battery_charger(self, **charging_parameters):
+    def battery_charger(self, **charging_configs):
         try:
+            test_cell = charging_configs['test_cell']
             #check if the emulator is on
-            emulator_status = int(self.emulator.query("OUTP:STAT?").strip('\n'))
+            emulator_status = int(self.emulator.query("OUTP:STAT?").strip('/n'))
             print(f"The status of the emulator is {emulator_status}")
 
             if emulator_status == 0:
                 #Clear system errors.
+                print("Clearing errors")
                 self.emulator.write("SYST:CLE")
 
                 #Check if the instrument is already on battery mode if so. 
-                if int(self.emulator.query("BATT?").strip('\n'))  == 1:
+                if int(self.emulator.query("BATT?").strip('/n'))  == 1:
+                        print("==========================Switching off battery mode for a fresh start=================")
                         self.emulator.write("BATT 0")
                 
+                print("==========================Setting emulator to source mode=================")
                 self.emulator.write("SYST:FUNC SOUR")
+
+                print("==========================Setting emulator to Battery mode=================")
                 self.emulator.write("BATT:MODE CHAR")
                 
+                print(f"==========================Setting battery charging voltage to {charging_configs["charge_voltage"]} =================")
+                self.emulator.write(f"BATT:CHAR:VOLT {charging_configs["charge_voltage"]}")
+
+                print(f"==========================Setting battery charging current to: {charging_configs["charge_current"]} =================")
+                self.emulator.write(f"BATT:CHAR:CURR {charging_configs["charge_current"]}")
+
+                print(f"==========================Setting battery cut off voltage to: {charging_configs["stop_voltage"]} =================")
+                self.emulator.write(f"BATT:STOP:VOLT {charging_configs['stop_voltage']}")
                 
-                self.emulator.write(f"BATT:CHAR:VOLT {charging_parameters["charge_voltage"]}")
-                self.emulator.write(f"BATT:CHAR:CURR {charging_parameters["charge_current"]}")
+                print(f"==========================Setting battery cutt off current to: {charging_configs["stop_current"]} =================")
+                self.emulator.write(f"BATT:STOP:CURR {charging_configs['stop_current']}")
 
+                print(f"==========================Setting battery cut off capacity to: {charging_configs["stop_capacity"]} =================")
+                self.emulator.write(f"BATT:STOP:CAP {charging_configs['stop_capacity']}")
 
-                self.emulator.write(f"BATT:STOP:VOLT {charging_parameters['stop_voltage']}")
-                self.emulator.write(f"BATT:STOP:CURR {charging_parameters['stop_current']}")
-                self.emulator.write(f"BATT:STOP:CAP {charging_parameters['stop_capacity']}")
-
-                self.emulator.write(f"BATT:STOP:TIME {charging_parameters['stop_time']}")
+                print(f"==========================Setting battery cut off time: {charging_configs["stop_time"]} =================")
+                self.emulator.write(f"BATT:STOP:TIME {charging_configs['stop_time']}")
                 
 
                 #Check if the voltage has been set successfully
@@ -84,42 +109,105 @@ class PVStorageEmulator:
                 print(f"Charging current is: {current}")
 
 
-
+                print(f"==========================Now switching on the emulator to start charging =================")
                 self.emulator.write("BATT 1")
                 self.emulator.write("OUTPUT ON")
 
                 # if ((voltage <= CELL_MAX_VOLTAGE) and (voltage == charge_voltage)) and ((current <= CELL_MAX_CURRENT) and (current == charge_current)) :
                 #     self.emulator.write("OUTPUT ON")
 
+                output_status = int(self.emulator.query("OUTPUT?").strip("/n"))
+
+                battery_charging_data = {} 
+                record_key = 0
+
+                while output_status == 1:
+                    record = {}
+                    charge_record_key = test_cell+"char00"+str(record_key)
+                    record['Time_stamp'] = datetime.datetime.now()
+                    record['Cell_voltage'] = self.emulator.query("MEAS:VOLT?")
+                    record['Cell_current'] = self.emulator.query("MEAS:CURR?")
+                    record['Cell_charging_capacity'] = self.emulator.query("MEAS:CAP?")
+                    record['Ambient_temperature'] = self.emulator.query("MEAS:TEMP?")
+                    battery_charging_data[charge_record_key] = record
+                    record_key += 1
+                    output_status = int(self.emulator.query("OUTPUT?").strip("/n"))
+
+                with open(f"{charging_configs['charge_data_path']}/cell_{test_cell}_dicharging_data_{datetime.datetime.now()}.json", "w") as outfile:
+                    json.dump(battery_charging_data, outfile)
+
+                print("Checking if any errors were generated during system configuration")
                 print("ERROR", self.emulator.query("SYST:ERR?"))
 
         except Exception as e:
             print(f"Failed to set up the instrument for the charging process because of this error: {e}")
 
 
-    def battery_discharge(self, discharge_voltage, discharge_current):
+    def battery_discharge(self, **discharge_configs):
         try:
             #check if the emulator is on
-            emulator_status = int(self.emulator.query("OUTP:STAT?").strip('\n'))
+            test_cell = discharge_configs['test_cell']
+            emulator_status = int(self.emulator.query("OUTP:STAT?").strip('/n'))
 
             if emulator_status == 0:
                 #Set the device to act as a batetry charger and set the charging values.
-                self.emulator.write("SYST:FUNC SOUR")
                 #Check if the instrument is already on battery mode if so. 
-                if int(self.emulator.query("BATT?").strip('\n'))  == 0:
-                    self.emulator.write("BATT 1")
+                if int(self.emulator.query("BATT?").strip('/n'))  == 1:
+                    self.emulator.write("BATT 0")
+                
+                print("==========================Setting emulator to Battery mode=================")
+                self.emulator.write("BATT:MODE DISC")
 
-                self.emulator.write(f"BATT:CHAR:VOLT {discharge_voltage}")
-                self.emulator.write(f"BATT:CHAR:CURR {discharge_current}")
+                print(f"==========================Setting battery charging voltage to {discharge_configs["discharge_voltage"]} =================")
+                self.emulator.write(f"BATT:DISC:VOLT {discharge_configs['discharge_voltage']}")
+
+                print(f"==========================Setting battery charging current to {discharge_configs["discharge_current"]} =================")
+                self.emulator.write(f"BATT:DISC:CURR {discharge_configs['discharge_current']}")
+
+                print(f"==========================Setting battery cut off voltage to {discharge_configs["stop_voltage"]} =================")
+                self.emulator.write(f"BATT:STOP:VOLT {discharge_configs['stop_voltage']}")
+
+                print(f"==========================Setting battery cut off current to {discharge_configs["stop_current"]} =================")
+                self.emulator.write(f"BATT:STOP:CURR {discharge_configs['stop_current']}")
+
+                print(f"==========================Setting battery cut off capacity to {discharge_configs["stop_capacity"]} =================")
+                self.emulator.write(f"BATT:STOP:CAP {discharge_configs['stop_capacity']}")
+                
+                print(f"==========================Setting battery cut off time {discharge_configs["stop_time"]} =================")
+                self.emulator.write(f"BATT:STOP:TIME {discharge_configs['stop_time']}")
+
+                print(f"==========================Now turning the Battery charge and output on emulator on to begin discharge process =================")
+                self.emulator.write("BATT 1")
+                self.emulator.write("OUTPUT 1")
 
                 #Check if the voltage has been set successfully
-                voltage = self.emulator.query('BATT:CHAR:VOLT?').strip('\n')
-                print(f"Charging voltage is: {voltage}").strip("\n")
-                current = self.emulator.query('BATT:CHAR:CURR?')
-                print(f"Charging current is: {current}")
+                print("===========================Checking if the discharging voltage and current are set properly======================================")
+                voltage = self.emulator.query('BATT:DISC:VOLT?').strip('/n')
+                print(f"Discharging voltage is: {voltage}").strip("/n")
+                current = self.emulator.query('BATT:DISC:CURR?')
+                print(f"Discharging current is: {current}")
 
-                if ((voltage <= CELL_MAX_VOLTAGE) and (voltage == discharge_voltage)) and ((current <= CELL_MAX_CURRENT) and (current == discharge_current)) :
-                    self.emulator.write("OUTPUT ON")
+                #Measure and log the discharging data
+                output_status = int(self.emulator.query("OUTPUT?").strip("/n"))
+
+                battery_discharging_data = {} 
+                record_key = 0
+
+                while output_status == 1:
+                    record = {}
+                    discharge_record_key = test_cell+"disc"+"00"+str(record_key)
+                    record['Time_stamp'] = datetime.datetime.now()
+                    record['Cell_disc_voltage'] = self.emulator.query("MEAS:VOLT?")
+                    record['Cell_disc_current'] = self.emulator.query("MEAS:CURR?")
+                    record['Cell_disc_capacity'] = self.emulator.query("MEAS:CAP?")
+                    record['Ambient_temperature'] = self.emulator.query("MEAS:TEMP?")
+                    battery_discharging_data[discharge_record_key] = record
+                    record_key += 1
+                    output_status = int(self.emulator.query("OUTPUT?").strip("/n"))
+
+            with open(f"{discharge_configs['discharge_data_path']}/cell_{test_cell}_dicharging_data_{datetime.datetime.now()}.json", "w") as outfile:
+                    json.dump(battery_discharging_data, outfile)
+
 
         except Exception as e:
             print(f"Failed to set up the instrument for the charging process because of this error: {e}")
@@ -148,25 +236,50 @@ class PVStorageEmulator:
 
 def main(**kwaargs):
 
-    #weather_data_paths = "C:\Users\yamzi\source_code\msc_research\rul_prediction\src\datasets\weather-2021.csv"
-    charge_current = 4.2
-    charge_voltage = 1.0
-    load_voltage = 12.0
-    load_current = 1.0
+    #weather_data_paths = "C:/Users/yamzi/source_code/msc_research/rul_prediction/src/datasets/weather-2021.csv"
+    config_path = "C:/Users/yamzi/source_code/msc_research/rul_prediction/src/python_code/configs/BAT_CELL_BL26650.json"
+    battery_test_path = "C:/Users/yamzi/source_code/msc_research/rul_prediction/src/datasets"
+    battery_test_cell = config['test']['Battery_cell']
 
-    solar_emulator = PVStorageEmulator(REGEN_POWER_SUPPLY_ADDR_ITECH_M3622)
+    with open(config_path, 'r') as config_file:
+        config = json.load(config_file)
+
+    solar_emulator = PVStorageEmulator(config['test']['test_inst_addr'])
+
+    charging_configs = config['test']['Charge']
+
+    discharging_configs = config['test']['Discharge']
+
+    print("==========================Starting Charging using the following Parameters============================")
+    print(charging_configs)
     #solar_emulator.get_device_status()
-    solar_emulator.battery_charger(charge_voltage = 3.7,
-                                   charge_current = 3.4,
-                                   stop_voltage = 4.2,
-                                   stop_current = 5.0,
-                                   stop_capacity = 5.44,
-                                   stop_time = 120
+    solar_emulator.battery_charger(
+                                   charge_data_path = battery_test_path,
+                                   test_cell = battery_test_cell,
+                                   charge_voltage = charging_configs['charge_voltage'],
+                                   charge_current = charging_configs['charge_current'],
+                                   stop_voltage = charging_configs['stop_voltage'],
+                                   stop_current = charging_configs['stop_current'],
+                                   stop_capacity = charging_configs['stop_capacity'],
+                                   stop_time = charging_configs['stop_time']
                                    )
-    #time.sleep(3600)
-    #solar_emulator.battery_discharge(load_voltage, load_current)
-    
+     
     #wait for the charging process to finish
+    print("=================================================Charging is done ==========================================")
+
+    print("==========================Now Discharging harging using the following Parameters============================")
+    print(discharging_configs)
+
+    solar_emulator.battery_discharge(
+                                   discharge_data_path = battery_test_path,
+                                   test_cell = battery_test_cell,
+                                   discharge_voltage = discharging_configs['discharge_voltage'],
+                                   discharge_current = discharging_configs['discharge_current'],
+                                   stop_voltage = discharging_configs['stop_voltage'],
+                                   stop_current = discharging_configs['stop_current'],
+                                   stop_capacity = discharging_configs['stop_capacity'],
+                                   stop_time = discharging_configs['stop_time']
+    )
 
 
 if __name__ == "__main__":
