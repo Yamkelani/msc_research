cf66aa9f0ff381cca41d457394bef857d0c252ad
Yamkelani <yamzinm@gmail.com>
2024-11-12 00:03:54 +0200

    weather data analysis code is now able to generate the data for the 4 seasons from the given data.

--- rul_prediction/src/python_code/data_analysis/weather_data_analysis.py
+++ rul_prediction/src/python_code/data_analysis/weather_data_analysis.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 
 CALENDAR = {
     1:"January",
@@ -37,17 +38,31 @@ def main():
     weather_data['IrraSlope'] = 0
     weather_data['TempSlope'] = 0
     weather_data['Sec'] = 60
-    weather_data.rename(columns={"Temperature": "Temp", "Solar_irradiation": "Irra"})
+    weather_data = weather_data.rename(columns={"Temperature": "Temp", "Solar_irradiation": "Irra"})
 
 
 
 
     summer = weather_data[(weather_data['Month'] == 'January') | (weather_data['Month'] == 'February') | (weather_data['Month'] == 'December')]
-    summer = summer[['IrraSlope','TempSlope','Sec','Temp','Irra']]
+    summer['Index'] = np.arange(1, len(summer)+1)
+    summer = summer[['Index','IrraSlope','TempSlope','Sec','Temp','Irra']]
+    summer.to_csv(output_path+"summer_weather_data.csv", index=False)
+    print(len(summer))
     winter = weather_data[(weather_data['Month'] == 'June') | (weather_data['Month'] == 'July') | (weather_data['Month'] == 'August')]
+    winter['Index'] = np.arange(1, len(winter)+1)
+    winter = winter[['Index','IrraSlope','TempSlope','Sec','Temp','Irra']]
+    winter.to_csv(output_path+"winter_weather_data.csv", index=False)
+    print(len(winter))
     autum  = weather_data[(weather_data['Month'] == 'March') | (weather_data['Month'] == 'April') | (weather_data['Month'] == 'May')]
-    sprint = weather_data[(weather_data['Month'] == 'September') | (weather_data['Month'] == 'October') | (weather_data['Month'] == 'November')]
-
+    autum['Index'] = np.arange(1, len(autum)+1)
+    autum = autum[['Index','IrraSlope','TempSlope','Sec','Temp','Irra']]
+    autum.to_csv(output_path+"autum_weather_data.csv", index=False)
+    print(len(autum))
+    spring = weather_data[(weather_data['Month'] == 'September') | (weather_data['Month'] == 'October') | (weather_data['Month'] == 'November')]
+    spring['Index'] = np.arange(1, len(spring)+1)
+    spring = spring[['Index','IrraSlope','TempSlope','Sec','Temp','Irra']]
+    spring.to_csv(output_path+"spring_weather_data.csv", index=False)
+    print(len(spring))
 
     print(summer[:5])
 
