9090552a578e2bbe537738d6ae7c83bfe88b7015
Yamkelani <yamzinm@gmail.com>
2024-11-12 11:38:21 +0200

    Updated the weather data analysis logic

--- rul_prediction/src/python_code/data_analysis/weather_data_analysis.py
+++ rul_prediction/src/python_code/data_analysis/weather_data_analysis.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+from statistics import mean
 
 CALENDAR = {
     1:"January",
@@ -17,6 +18,19 @@ CALENDAR = {
 
 }
 
+def aggregate_data(data, output_path, season):
+    data = data.groupby('Date',as_index = False).agg({"Irra":mean, "Temp":mean})
+    data['Index'] = np.arange(1, len(data)+1)
+    data['IrraSlope'] = 0
+    data['TempSlope'] = 0
+    data['Sec'] = 60
+    data = data[['Index','IrraSlope','TempSlope','Sec','Temp','Irra']]
+
+    print(f"Wtring data for the {season} season.")
+    data.to_csv(output_path+f"{season}_weather_data.csv", index=False)
+    print(len(data))
+    print(data[:5])
+
 
 def main():
     base_path = "C:/Users/yamzi/source_code/msc_research/rul_prediction/src/datasets/old_data_sets/"
@@ -26,7 +40,6 @@ def main():
 
     weather_data_2021_subset = weather_data_2021[['Timestamp (UTC+0)','Solar_irradiation', 'Temperature']]
     weather_data_2022_subset = weather_data_2022[['Timestamp (UTC+0)','Solar_irradiation', 'Temperature']]
-
     data_frames = [weather_data_2021_subset, weather_data_2022_subset]
 
     weather_data = pd.concat(data_frames)
@@ -34,37 +47,23 @@ def main():
     weather_data['Timestamp (UTC+0)'] = pd.to_datetime(weather_data['Timestamp (UTC+0)'])
     weather_data['Year'] = weather_data['Timestamp (UTC+0)'].dt.year
     weather_data['Month'] = weather_data['Timestamp (UTC+0)'].dt.month
+    weather_data['Date'] = weather_data['Timestamp (UTC+0)'].dt.date
     weather_data['Month'] = weather_data['Month'].apply(lambda x : CALENDAR[x])
-    weather_data['IrraSlope'] = 0
-    weather_data['TempSlope'] = 0
-    weather_data['Sec'] = 60
     weather_data = weather_data.rename(columns={"Temperature": "Temp", "Solar_irradiation": "Irra"})
 
 
 
-
     summer = weather_data[(weather_data['Month'] == 'January') | (weather_data['Month'] == 'February') | (weather_data['Month'] == 'December')]
-    summer['Index'] = np.arange(1, len(summer)+1)
-    summer = summer[['Index','IrraSlope','TempSlope','Sec','Temp','Irra']]
-    summer.to_csv(output_path+"summer_weather_data.csv", index=False)
-    print(len(summer))
+    aggregate_data(summer, output_path, "summer")
+    
     winter = weather_data[(weather_data['Month'] == 'June') | (weather_data['Month'] == 'July') | (weather_data['Month'] == 'August')]
-    winter['Index'] = np.arange(1, len(winter)+1)
-    winter = winter[['Index','IrraSlope','TempSlope','Sec','Temp','Irra']]
-    winter.to_csv(output_path+"winter_weather_data.csv", index=False)
-    print(len(winter))
+    aggregate_data(winter, output_path, "winter")
+    
     autum  = weather_data[(weather_data['Month'] == 'March') | (weather_data['Month'] == 'April') | (weather_data['Month'] == 'May')]
-    autum['Index'] = np.arange(1, len(autum)+1)
-    autum = autum[['Index','IrraSlope','TempSlope','Sec','Temp','Irra']]
-    autum.to_csv(output_path+"autum_weather_data.csv", index=False)
-    print(len(autum))
-    spring = weather_data[(weather_data['Month'] == 'September') | (weather_data['Month'] == 'October') | (weather_data['Month'] == 'November')]
-    spring['Index'] = np.arange(1, len(spring)+1)
-    spring = spring[['Index','IrraSlope','TempSlope','Sec','Temp','Irra']]
-    spring.to_csv(output_path+"spring_weather_data.csv", index=False)
-    print(len(spring))
+    aggregate_data(autum, output_path, "autum")
 
-    print(summer[:5])
+    spring = weather_data[(weather_data['Month'] == 'September') | (weather_data['Month'] == 'October') | (weather_data['Month'] == 'November')]
+    aggregate_data(spring,output_path, "spring")
 
 if __name__ == "__main__":
     main()
