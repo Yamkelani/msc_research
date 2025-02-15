55fcf0d6b32969481ceed1c0d448d6f8d4812b56
Yamkelani <yamzinm@gmail.com>
2025-02-16 00:10:32 +0200

    updated data preprocessing scripts

--- rul_prediction/src/matlab_code/scripts_new/prepare_rul_sensor_data.m
+++ rul_prediction/src/matlab_code/scripts_new/prepare_rul_sensor_data.m
@@ -1,15 +1,15 @@
 %This code reads in mat lab files that contain the data logs from simulink and 
 % restructures then saves the data in a csv for further processing.
-battery_model = "IFR32700"
-cell_number = "cell2"
+battery_model = "IFR32700";
+cell_number = 'cell1';
 prepare_matlab_data(cell_number, battery_model)
 
 function prepare_matlab_data(cell_number, model)    
-    base_dir = "/home/yamukelani/source_code/msc_research/rul_prediction/src/datasets/battery_experiments_data/";
+    base_dir = '/home/yamukelani/source_code/msc_research/rul_prediction/src/datasets/battery_experiments_data/';
 
-    matlab_path = "cell2/matlab/";
-    src_dir = base_dir+matlab_path;
-    dest_path = base_dir+matlab_path+'csv_files/';
+    matlab_path = [cell_number,'/matlab/many_cycles/'];
+    src_dir = [base_dir, matlab_path]
+    dest_path = [base_dir, cell_number ,'/matlab/csv_files/'];
     
     
     mat_files_list = dir(fullfile(src_dir, '*.mat'));
@@ -26,9 +26,14 @@ function prepare_matlab_data(cell_number, model)
         
         %Display filename before processing it for prgress tracking. 
         fprintf('File to load:  %s\n', file_name);
-        batt_test_data = load(file_path);
-        logged_data = batt_test_data.logsout;
         
+        batt_test_data = load(file_path)
+        
+        if isfield(batt_test_data, 'logsout')
+            logged_data = batt_test_data.logsout;
+        else
+            logged_data = batt_test_data.data
+        end
 
         target_file_name = strrep(file_name, '.mat', '');
         processed_data = preprocess_data(logged_data);
@@ -36,7 +41,7 @@ function prepare_matlab_data(cell_number, model)
         processed_data.cell_number = repmat(split_file_name{4}, height(processed_data), 1);
         processed_data.cycle =  repmat(split_file_name{2}, height(processed_data), 1);
         processed_data.date =  repmat(cycle_date, height(processed_data), 1);
-        writetable(processed_data,dest_path+target_file_name+'.csv');
+        writetable(processed_data,[dest_path,target_file_name,'.csv']);
     
     end
 end
--- rul_prediction/src/matlab_code/scripts_new/process_cycles_data.m
+++ rul_prediction/src/matlab_code/scripts_new/process_cycles_data.m
@@ -2,20 +2,26 @@ parent_dir = '/home/yamukelani/source_code/msc_research/rul_prediction/src/datas
 cell = '3'
 matlab_path = [parent_dir, 'unprocess_matlab_data/'];
 csv_path = [parent_dir, 'unprocessed_data/'];
-consolidated_path = [parent_dir, 'consolidated_data/consolidated_test_data_cell_',cell, '.csv']
+charge_path = [parent_dir, 'consolidated_data/charge_consolidated_test_data_cell_',cell, '.csv']
+discharge_path = [parent_dir, 'consolidated_data/discharge_consolidated_test_data_cell_',cell, '.csv']
 
 mat_files_list = dir(fullfile(matlab_path, '*.mat'));
 csv_files_list = dir(fullfile(csv_path, '*.csv'))
-data = process_csv_files(csv_path, csv_files_list);
-writetable(data, consolidated_path)
+[charge_data, discharge_data] = process_csv_files(csv_path, csv_files_list);
+writetable(charge_data, charge_path)
+writetable(discharge_data, discharge_path)
 
 
-function em_data = process_csv_files(dir, paths)
+function [charging_data, discharging_data] = process_csv_files(dir, paths)
     charge_columns = {'Battery_cell_model','Test_cell_number','Operation', 'Cycle', 'Time_stamp', 'Measured_cell_voltage',...
         'Measured_cell_current','Measured_cell_capacity', 'Charging_voltage',  'Charging_current', 'Ambient_temperature',...
          'Cell_temperature' }
+    discharge_columns = {'Battery_cell_model','Test_cell_number','Operation', 'Cycle', 'Time_stamp', 'Measured_cell_voltage',...
+            'Measured_cell_current','Measured_cell_capacity', 'Discharging_voltage',  'Discharging_current', 'Ambient_temperature',...
+             'Cell_temperature' }
 
     charging_data = table([],[],[],[],[],[],[],[],[],[],[],[], 'VariableNames', charge_columns)
+    discharging_data = table([],[],[],[],[],[],[],[],[],[],[],[], 'VariableNames', discharge_columns)
 
     for i = 1:length(paths)
         file_path = fullfile(dir, paths(i).name)
@@ -30,10 +36,14 @@ function em_data = process_csv_files(dir, paths)
             charge_data.Properties.VariableNames;
             charging_data = [charging_data; charge_data];
         end
+        if contains(file_path, '_discharging')
+            discharge_data = readtable(file_path, opts);
+            discharge_data(:, 'Var1') = [];
+            discharge_data.Properties.VariableNames
+            discharging_data = [discharging_data; discharge_data];
+        end
 
 
     end
-
-    em_data = charging_data;
 
 end
